c2b4a014487f71c7aacaec623d7631992ad77b4e
MuchiriKinyua <muchirikinyua2000@gmail.com>
2023-09-18 06:24:21 +0300

    renaming project

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       translations: AppLocalization(),
       locale: Get.deviceLocale, //for setting localization strings
       fallbackLocale: Locale('en', 'US'),
-      title: 'tusome',
+      title: 'tusome app',
       initialBinding: InitialBindings(),
       initialRoute: AppRoutes.initialRoute,
       getPages: AppRoutes.pages,
